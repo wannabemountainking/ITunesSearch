4922ce52dd24951792b2715c3c259f0e6303b6cf
wannabemountainking <kdy0096@gmail.com>
2026-03-19 10:19:07 +0900

    Initial Commit

---
+++ ITunesSearch/ITunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesSearchApp.swift
+//  ITunesSearch
+//
+//  Created by YoonieMac on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ITunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
